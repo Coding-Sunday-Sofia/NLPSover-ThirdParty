cb72637eb752c7a0e6bfeaf88ad3590cc27529c5
Todor Balabanov <todor.balabanov@gmail.com>
2019-05-01 22:05:46 +0300

    Building object model for testing was not successful.

--- ThirdParty/EvolutionarySolver/src/Main.java
+++ ThirdParty/EvolutionarySolver/src/Main.java
@@ -1,12 +1,60 @@
 import java.util.Arrays;
 
+import net.adaptivebox.deps.behavior.DEGTBehavior;
 import net.adaptivebox.global.RandomGenerator;
+import net.adaptivebox.knowledge.Library;
+import net.adaptivebox.knowledge.SearchPoint;
+import net.adaptivebox.problem.ProblemEncoder;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		final long NUMBER_OF_EXPERIMENTS = 5_000_000L;
 
-		for (int h = 0; h < 5; h++) {
+		/* First algorithm. */ {
+			DEGTBehavior behavior = new DEGTBehavior();
+
+			ProblemEncoder problemEncoder = new ProblemEncoder(10, 10) {
+				@Override
+				protected double calcTargetAt(int index, double[] VX) {
+					return Math.random();
+				}
+
+				@Override
+				public String toString() {
+					setDefaultXAt(0, 0, 100, 0.01);
+					setDefaultXAt(1, 0, 100, 0.01);
+					setDefaultXAt(2, 0, 100, 0.01);
+					setDefaultXAt(3, 0, 100, 0.01);
+					setDefaultXAt(4, 0, 100, 0.01);
+					setDefaultXAt(5, 0, 100, 0.01);
+					setDefaultXAt(6, 0, 100, 0.01);
+					setDefaultXAt(7, 0, 100, 0.01);
+					setDefaultXAt(8, 0, 100, 0.01);
+					setDefaultXAt(9, 0, 100, 0.01);
+					return "";
+				}
+			};
+
+			problemEncoder.toString();
+			
+			SearchPoint pbest = problemEncoder.getEncodedSearchPoint();
+
+			SearchPoint trailPoint = problemEncoder.getFreshSearchPoint();
+
+			Library library = new Library(100, problemEncoder);
+
+			behavior.setLibrary(library);
+			behavior.setPbest(pbest);
+
+			long start = System.currentTimeMillis();
+			for (long g = 0; g < NUMBER_OF_EXPERIMENTS; g++) {
+				behavior.generateBehavior(trailPoint, problemEncoder);
+			}
+			long stop = System.currentTimeMillis();
+			System.out.println(stop - start);
+		}
+
+		for (int h = 0; h < 0; h++) {
 			int a = (int) (10 + Math.random() * 10);
 			int b = (int) (2 + Math.random() * 10);
 			System.out.println(Arrays.toString(RandomGenerator.randomSelection(a, b)));
@@ -59,7 +107,7 @@ public class Main {
 				long stop = System.currentTimeMillis();
 				System.out.println(stop - start);
 			}
-			
+
 			System.out.println();
 		}
 	}
